e79851dd12cd14ed4c158279fa5d3af8858ad925
juliaecaro <juliaecaro@gmail.com>
2024-06-02 21:22:29 -0500

    committing project

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+// Last edited by: Julia Caro
+// Last edited on: June 2, 2024
+
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
